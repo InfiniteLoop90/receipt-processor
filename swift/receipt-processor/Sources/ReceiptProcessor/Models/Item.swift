a9026967d8decbeeb5f865179109d7b52a1448a3
Clayton Bodendein <clayton.bodendein@gmail.com>
2026-02-25 17:27:47 -0600

    Got POST working by treating the price as a string for decoding

--- swift/receipt-processor/Sources/ReceiptProcessor/Models/Item.swift
+++ swift/receipt-processor/Sources/ReceiptProcessor/Models/Item.swift
@@ -3,5 +3,14 @@ import Vapor
 
 struct Item: Content {
     let shortDescription: String
-    let price: Decimal
+    let priceString: String  // Use a string property for the raw data
+    // Computed property to access as Decimal
+    var price: Decimal? {
+        return Decimal(string: priceString)
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case shortDescription
+        case priceString = "price" // Map the JSON "price" key to this string property
+    }
 }
