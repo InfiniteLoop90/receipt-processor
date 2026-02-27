dfa5efd9e242bd080ca4d8a21e07ac4d88f8ba41
Clayton Bodendein <clayton.bodendein@gmail.com>
2026-02-26 21:57:25 -0600

    Changing the index leaf

--- swift/receipt-processor/Sources/ReceiptProcessor/Services/ReceiptStoreKey.swift
+++ swift/receipt-processor/Sources/ReceiptProcessor/Services/ReceiptStoreKey.swift
@@ -1,7 +1,7 @@
 import Vapor
 
-/*
-   Got this idea from https://docs.vapor.codes/advanced/services/#writable
+/**
+ Got this idea from https://docs.vapor.codes/advanced/services/#writable
  */
 struct ReceiptStoreKey: StorageKey {
     typealias Value = ReceiptStore
--- swift/receipt-processor/Sources/ReceiptProcessor/routes.swift
+++ swift/receipt-processor/Sources/ReceiptProcessor/routes.swift
@@ -1,8 +1,18 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    let now = Date()
+    let dateFormatter = DateFormatter()
+    dateFormatter.timeZone = TimeZone.current
+    dateFormatter.dateStyle = .long
+    dateFormatter.timeStyle = .long
+    let localTimeStr = dateFormatter.string(from: now)
+
     app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
+        try await req.view.render("index", [
+            "title": "Receipt Processor Home Page (Swift + Vapor)",
+            "currentTimestamp": localTimeStr
+        ])
     }
 
     try app.register(collection: ReceiptsController())
